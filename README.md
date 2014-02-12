c0c1fd86b5e50504f950ee4b1e39fc61a7aecced
awiede <andreas.wiede@student.american.edu>
2014-02-11 22:27:39 -0500

    remove test prints and edited README

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-Shell
+Andreas's Shell
 =====
+This is a basic shell that can handle some basic commands on its own and sends others to the bash shell.
+Upon startup you will be greated with a welcome, and when you see "awiede$ " you can enter your command. 
+
+Basic Commands:
+=====
+
+exit 
+This exits the shell.
+
+type <filename> 
+This reads in a file and prints out its contents.
+
+copy <file1> <file2> 
+This reads in the first file and copies it to the second file.
+If the second file exists it is erased and replaced with the contents of the first.
+If the second file does not exist the shell creates the file.
+
+delete <filename>
+This deletes the file.
+
+Other Commands:
+=====
+If you enter a command that is not on the basic commands list, but is an executable command
+in the bash shell, then the shell will run it. 
--- shell.c
+++ shell.c
@@ -39,7 +39,6 @@ void shell(){
     int i;
     int a;
     int c;
-    int rem;
     int pid;
     int size;
     char file_char;
@@ -58,18 +57,12 @@ void shell(){
                 i=101;
             }
         }
-        printf("end for loop\n");
-
-        printf("The input is %s and nothing else. \n",line);
-        //printf("strcmp of line and exit is %i\n", strcmp(line,'exit'));
         if(strcmp(line,"exit")==0)
         {
-            printf("Terminating Shell\n");
             break;
         }
         else if(strncmp(line,"type",4)==0)
         {
-            printf("In the type if statement\n");
             char command[4];
             char file_name[50];
             char* pieces[2];
@@ -77,11 +70,8 @@ void shell(){
             pieces[1]=file_name;
 
             split(line, pieces);
-            printf("pieces[0] is: %s\n", pieces[0]);
-            printf("pieces[1] is: %s\n", pieces[1]);
 
             fp=fopen(pieces[1],"r");
-            //fprintf(stderr, "%s\n", );
             while(1)
             {
                 c = fgetc(fp);
@@ -94,7 +84,6 @@ void shell(){
         }
         else if (strncmp(line,"copy",4)==0)
         {
-            printf("In the Copy if statement.\n");
             char command[4];
             char file_name1[50];
             char file_name2[50];
@@ -103,14 +92,8 @@ void shell(){
             pieces[1]=file_name1;
             pieces[2]=file_name2;
             split(line, pieces);
-
-            printf("pieces[0]: %s\n", pieces[0]);
-            printf("pieces[1]: %s\n", pieces[1]);
-            printf("pieces[2]: %s\n", pieces[2]);
-
             fp=fopen(pieces[1],"r");
             wf=fopen(pieces[2],"w");
-
             while(1)
             {
                 c = fgetc(fp);
@@ -123,26 +106,19 @@ void shell(){
         }
         else if(strncmp(line, "delete",6)==0)
         {
-            printf("In the delete if statement\n");
             char command[6];
             char file_name[50];
             char* pieces[2];
             pieces[0]=command;
             pieces[1]=file_name;
             split(line,pieces);
-            printf("pieces[1]: %s\n", pieces[1]);
-            //fp=fopen(pieces[1],"w");
             remove(pieces[1]);
-            //rem=remove(fp);
-            //printf("Remove: %i\n", rem);
         }
         else
         {
-            printf("Inside the else statement. \n");
             pid = fork();
             if (pid==0)
-            {
-                
+            {    
                 char path[50];
                 char command1[50];
                 char command2[50];
@@ -157,24 +133,13 @@ void shell(){
                 size=0;
                 for(i=1;i<4; i++)
                 {
-                    printf("In for loop\n");
-                    printf("pieces i 0 %i\n", pieces[i][0]);
                     if (pieces[i][0]!=0)
                         size++;
                     else
                         break;
                 }
                 pieces[size+1]=0;
-                printf("pieces 0 %s\n", pieces[0]);
-                printf("pieces 1 %s\n", pieces[1]);
-                printf("peices 2 %s\n", pieces[2]);
-                printf("size %i\n", size);
-                //arg2[1]=pieces[2];
-                //arg2[2]='\0';
-                //args[0][0]='\0';
                 execvp(pieces[0], pieces);
-                
-                //execvp(line);
             }
             else if (pid!=0)
             {
